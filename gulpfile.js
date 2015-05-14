f777f36ae19d09b27dba3c975db2cfe264105cad
Daniel Treasure <dbtreasure@gmail.com>
2015-05-13 18:28:48 -0600

    creates a vendor file and slugignore

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,10 @@ gulp.task('html', function(){
     gulp.src(['src/partials/*.html']).pipe(livereload());
 });
 
+gulp.task('vendor', function(){
+    gulp.src('src/vendor/##/*').pipe(gulp.dest('dist/vendor'))
+});
+
 gulp.task('fonts', function() {
     gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'))
 })
@@ -71,7 +75,7 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('html', 'styles', 'scripts', 'images', 'fonts');
+    gulp.start('html', 'styles', 'scripts', 'images', 'fonts', 'vendor');
 });
 
 gulp.task('watch', function() {
@@ -84,6 +88,8 @@ gulp.task('watch', function() {
     gulp.watch('src/js/**/*.js', ['scripts']);
 
     gulp.watch('src/images/**/*', ['images']);
+
+    gulp.watch('src/vendor/**/*' ['vendor'])
 
     livereload.listen();
 

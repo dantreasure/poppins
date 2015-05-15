1d0020f16e107448293c2bf7af7e6494ec5a85c4
Daniel Treasure <dbtreasure@gmail.com>
2015-05-15 00:26:19 -0600

    adds functioning filters and ordering

--- src/js/staff-ctrl.js
+++ src/js/staff-ctrl.js
@@ -1,4 +1,31 @@
-sms.controller('staffCtrl', ['$scope', 'students', function($scope, students) {
+sms.controller('staffCtrl', ['$scope', '$http', '$filter', 'students', function($scope, $http, $filter, students) {
 	$scope.students = students.getStudents();
+	$scope.ordering = '';
 
+	$scope.mentor = '';
+
+	$scope.setOrder = function(order){
+		if($scope.ordering === order){
+			if($scope.ordering.charAt(0) !== '-'){
+				$scope.ordering = '-' + $scope.ordering;
+			} else{
+				$scope.ordering = '';
+			}
+		} else{
+			$scope.ordering = order;
+		}
+	};
+
+	$scope.setMentor = function(mentor){
+		$scope.mentor = mentor
+	};
+
+	$scope.$watch('mentor', function(newValue, oldValue) {
+		if(newValue !== ''){
+			$scope.students = $filter('filter')($scope.students, {mentor:$scope.mentor});
+		} else{
+			$scope.students = students.getStudents();
+		}
+
+	});
 }]);

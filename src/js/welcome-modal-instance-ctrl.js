db5c78ab32140484609ec370e294b5055e1e0d12
Daniel Treasure <dbtreasure@gmail.com>
2015-05-18 15:35:08 -0600

    spacing

--- src/js/welcome-modal-instance-ctrl.js
+++ src/js/welcome-modal-instance-ctrl.js
@@ -1,6 +1,7 @@
 sms.controller('WelcomeModalInstanceCtrl', ['$scope', '$modalInstance', '$cookies', function ($scope, $modalInstance, $cookies) {
 	$scope.close = function(){
 		$cookies.welcomed = true;
+
 		$modalInstance.close();
 	};
 }]);
